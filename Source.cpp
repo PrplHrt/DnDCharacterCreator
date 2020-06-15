d5a944598b4f670e779f7259b60c65be2f626035
Eyad Mohamed Ali <eyad.azim@gmail.com>
2020-06-15 18:37:45 +0400

    Allowed for input of lowercase letters on Race menu. Changed some formatting (Added \t-tabs) and fixed some issues. Added racial bonuses array and implemented the bonuses for races without subraces.

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,8 @@
-#include "cmath"
-#include "iostream"
-#include "ctime"
-#include "stdlib.h"
+#include <cmath>
+#include <iostream>
+#include <ctime>
+#include <stdlib.h>
+#include <cctype>
 
 //Using the handbook in this link to create this program: https://online.anyflip.com/ofsj/cxmj/mobile/index.html
 
@@ -225,32 +226,34 @@ void Race(Character& player)
     desc[0][0] = "Dwarf";       //Has Subraces
     desc[1][0] = "Elf";         //Has Subraces
     desc[2][0] = "Halfling";    //Has Subraces
-    desc[3][0] = "Human";
+    desc[3][0] = "Human";       //Has Variants
     desc[4][0] = "Dragonborn";  //Add Detail for Dragonborn race breaths
     desc[5][0] = "Gnome";       //Has Subraces
     desc[6][0] = "Half-Elf";
     desc[7][0] = "Half-Orc";
     desc[8][0] = "Tiefling";
     //Race descriptions
-    desc[0][1] = "Lifespan: ~350 years\nAdult at: 50 years\nSize: 4 to 5 ft tall, ~150 lbs, Medium\nAlignment Tendencies: Lawful Good\nSpeed: 25ft\nLanguages:\n\t>Common\n\t>Dwarvish";
-    desc[1][1] = "Lifespan: ~750 years\nAdult at: ~100 years\nSize: 5 to 6 ft tall, Slender, Medium\nAlignment Tendencies: Chaotic Good\nSpeed: 30ft\nLanguages:\n\t>Common\n\t>Elvish";
-    desc[2][1] = "Lifespan: ~150 years\nAdult at: 20 years\nSize: ~3 ft tall, ~40 lbs, Small\nAlignment Tendencies: Lawful Good\nSpeed: 25ft\nLanguages:\n\t>Common\n\t>Halfling";
-    desc[3][1] = "Lifespan: ~80 years\nAdult at: ~20 years\nSize: 5 to 6+ ft tall, Medium\nAlignment Tendencies: None\nSpeed: 30ft\nLanguages:\n\t>Common\n\t>Any Other Language"; //Should add a choice later on for human's additional language
-    desc[4][1] = "Lifespan: ~80 years\nAdult at: 15 years\nSize: 6+ ft tall, ~250 pounds, Medium\nAlignment Tendencies: Either Good or Bad\nSpeed: 30ft\nLanguages:\n\t>Common\n\t>Draconic";
-    desc[5][1] = "Lifespan: 350-500 years\nAdult at: 40 years\nSize: 3 to 4 ft tall, ~40 lbs, Small\nAlignment Tendencies: Good (Lawful or Chaotic based on profession)\nSpeed: 25ft\nLanguages:\n\t>Common\n\t>Gnomic";
-    desc[6][1] = "Lifespan: ~180 years\nAdult at: 20 years\nSize: 5 to 6 ft tall, Human-like, Medium \nAlignment Tendencies: Chaotic\nSpeed: 30ft\nLanguages: \n\t>Common\n\t>Elvish\n\t>One more language of choice";
-    desc[7][1] = "Lifespan: ~75 years\nAdult at: 14 years\nSize: 5 to 6+ ft tall, ~190 lbs, Medium\nAlignment Tendencies: Chaotic Evil\nSpeed: 30ft\nLanguages:\n\t>Common\n\t>Orc";
-    desc[8][1] = "Lifespan: ~80 years\nAdult at: ~20 years\nSize:  5 to 6+ ft tall, Human-like, Medium\nAlignment Tendencies: \nSpeed: 30ft\nLanguages:\n\t>Common\n\t>Infernal";
+    desc[0][1] = "\tLifespan: ~350 years\n\tAdult at: 50 years\n\tSize: 4 to 5 ft tall, ~150 lbs, Medium\n\tAlignment Tendencies: Lawful Good\n\tSpeed: 25ft\n\tLanguages:\n\t\t>Common\n\t\t>Dwarvish";
+    desc[1][1] = "\tLifespan: ~750 years\n\tAdult at: ~100 years\n\tSize: 5 to 6 ft tall, Slender, Medium\n\tAlignment Tendencies: Chaotic Good\n\tSpeed: 30ft\n\tLanguages:\n\t\t>Common\n\t\t>Elvish";
+    desc[2][1] = "\tLifespan: ~150 years\n\tAdult at: 20 years\n\tSize: ~3 ft tall, ~40 lbs, Small\n\tAlignment Tendencies: Lawful Good\n\tSpeed: 25ft\n\tLanguages:\n\t\t>Common\n\t\t>Halfling";
+    desc[3][1] = "\tLifespan: ~80 years\n\tAdult at: ~20 years\n\tSize: 5 to 6+ ft tall, Medium\n\tAlignment Tendencies: None\n\tSpeed: 30ft\n\tLanguages:\n\t\t>Common\n\t\t>Any Other Language"; //Should add a choice later on for human's additional language
+    desc[4][1] = "\tLifespan: ~80 years\n\tAdult at: 15 years\n\tSize: 6+ ft tall, ~250 pounds, Medium\n\tAlignment Tendencies: Either Good or Bad\n\tSpeed: 30ft\n\tLanguages:\n\t\t>Common\n\t\t>Draconic";
+    desc[5][1] = "\tLifespan: 350-500 years\n\tAdult at: 40 years\n\tSize: 3 to 4 ft tall, ~40 lbs, Small\n\tAlignment Tendencies: Good (Lawful or Chaotic based on profession)\n\tSpeed: 25ft\n\tLanguages:\n\t\t>Common\n\t\t>Gnomic";
+    desc[6][1] = "\tLifespan: ~180 years\n\tAdult at: 20 years\n\tSize: 5 to 6 ft tall, Human-like, Medium \n\tAlignment Tendencies: Chaotic\n\tSpeed: 30ft\n\tLanguages: \n\t\t>Common\n\t\t>Elvish\n\t\t>One more language of choice";
+    desc[7][1] = "\tLifespan: ~75 years\n\tAdult at: 14 years\n\tSize: 5 to 6+ ft tall, ~190 lbs, Medium\n\tAlignment Tendencies: Chaotic Evil\n\tSpeed: 30ft\n\tLanguages:\n\t\t>Common\n\t\t>Orc";
+    desc[8][1] = "\tLifespan: ~80 years\n\tAdult at: ~20 years\n\tSize:  5 to 6+ ft tall, Human-like, Medium\n\tAlignment Tendencies: Chaotic\n\tSpeed: 30ft\n\tLanguages:\n\t\t>Common\n\t\t>Infernal";
     //Racial Bonuses
-    desc[0][2] = "STR: \nDEX: \nCON: +2\nINT: \nWIS: \nCHA: ";
-    desc[1][2] = "STR: \nDEX: +2\nCON: \nINT: \nWIS: \nCHA: ";
-    desc[2][2] = "STR: \nDEX: +2\nCON: \nINT: \nWIS: \nCHA: ";
-    desc[3][2] = "STR: +1\nDEX: +1\nCON: +1\nINT: +1\nWIS: +1\nCHA: +1";
-    desc[4][2] = "STR: +2\nDEX: \nCON: \nINT: \nWIS: \nCHA: +1";
-    desc[5][2] = "STR: \nDEX: \nCON: \nINT: +2\nWIS: \nCHA: ";
-    desc[6][2] = "STR: \nDEX: \nCON: \nINT: \nWIS: \nCHA: +2\nAnd +1 to any two other stats";
-    desc[7][2] = "STR: +2\nDEX: \nCON: +1\nINT: \nWIS: \nCHA: ";
-    desc[8][2] = "STR: \nDEX: \nCON: \nINT: +1\nWIS: \nCHA: +2";
+    desc[0][2] = "\tSTR: \n\tDEX: \n\tCON: +2\n\tINT: \n\tWIS: \n\tCHA: ";
+    desc[1][2] = "\tSTR: \n\tDEX: +2\n\tCON: \n\tINT: \n\tWIS: \n\tCHA: ";
+    desc[2][2] = "\tSTR: \n\tDEX: +2\n\tCON: \n\tINT: \n\tWIS: \n\tCHA: ";
+    desc[3][2] = "\tSTR: +1\n\tDEX: +1\n\tCON: +1\n\tINT: +1\n\tWIS: +1\n\tCHA: +1";
+    desc[4][2] = "\tSTR: +2\n\tDEX: \n\tCON: \n\tINT: \n\tWIS: \n\tCHA: +1";
+    desc[5][2] = "\tSTR: \n\tDEX: \n\tCON: \n\tINT: +2\n\tWIS: \n\tCHA: ";
+    desc[6][2] = "\tSTR: \n\tDEX: \n\tCON: \n\tINT: \n\tWIS: \n\tCHA: +2\n\tAnd +1 to any two other stats"; //Add option to select stat for additional bonus
+    desc[7][2] = "\tSTR: +2\n\tDEX: \n\tCON: +1\n\tINT: \n\tWIS: \n\tCHA: ";
+    desc[8][2] = "\tSTR: \n\tDEX: \n\tCON: \n\tINT: +1\n\tWIS: \n\tCHA: +2";
+    //Array that contains values for Racial Bonuses:
+    int bonus[9][6] = {{0,0,2,0,0,0},{0,2,0,0,0,0},{0,2,0,0,0,0},{1,1,1,1,1,1},{2,0,0,0,0,1},{0,0,0,2,0,0},{0,0,0,0,0,2},{2,0,1,0,0,0},{0,0,0,1,0,2}};
     //Subraces
     desc[0][3] = "Subraces:\n\t>Hill Dwarf\n\t>Mountain Dwarf";
     //Hill: WIS +1 & Hit Point Max +1 and +1/lvl | Mountain: STR +2 & proficiency with light and medium armor
@@ -276,12 +279,12 @@ void Race(Character& player)
     //Racial additions
     desc[0][4] = "-Speed not reduced by heavy armor.\n-Has \"DarkVision\" and can see upto 60ft in dim light as though it was bright and in darkness as if it was dim. You can\'t discern colors in darkness.\n-Advantage on saving throws against poison and resistance against poison damage.\n-Proficiency with battleaxe, handaxe, throwing hammer, and warhammer.\nProficiency in the tools of your choice:\n\t>smith\'s tools\n\t>brewer\'s supplies\n\t>mason\'s tools\n-Add double your proficiency bonus on history checks related to the origin of stonework.\n-Hard consonants and guttural sounds spill over to whatever language a Dwarf tries to speak.";
     desc[1][4] = "-Change names upon adulthood\n-Has \"DarkVision\" and can see upto 60ft in dim light as though it was bright and in darkness as if it was dim. You can\'t discern colors in darkness.\n-Proficiency in perception.\n-Advantage on saving throws against being charmed, and magic can't put you to sleep.\n-Does not sleep, instead can meditate for 4 hours and gains same benefit as 8 hours sleep.";
-    desc[2][4] = "-When you roll a 1 on a roll, you can roll again and must take the new roll.\n-Advantage on saving thorws from beign frightened.\n-You can move through any space of a creature that's size is larger than yours.";
+    desc[2][4] = "-When you roll a 1 on any roll, you can roll again and must take the new roll.\n-Advantage on saving throws from being frightened.\n-You can move through any space of a creature that's size is larger than yours.";
     desc[3][4] = "";
     desc[4][4] = "-Dragonborn posses a breath attack and resistances that vary based on the chosen type";
     desc[5][4] = "-Has \"DarkVision\" and can see upto 60ft in dim light as though it was bright and in darkness as if it was dim. You can\'t discern colors in darkness.\n-Advantage on all intelligence, charisma, and wisdom saving throws against magic";
     desc[6][4] = "-Has \"DarkVision\" and can see upto 60ft in dim light as though it was bright and in darkness as if it was dim. You can\'t discern colors in darkness.\n-Advantage on saving throws against being charmed, and magic can't put you to sleep.\n-Gain proficiency in n=any two skills of your choice.";
-    desc[7][4] = "-Has \"DarkVision\" and can see upto 60ft in dim light as though it was bright and in darkness as if it was dim. You can\'t discern colors in darkness.\n-Gain proficiency in the intimidation skill.\n-When dropping to 0 hit points but not killed outright, can drop to 1 hit point (Once after every long rest).\n-When hitting a critical hit with a melee weapon, roll one of the weapon's damage dice and add that as damage to the critical hit.";
+    desc[7][4] = "-Has \"DarkVision\" and can see upto 60ft in dim light as though it was bright and in darkness as if it was dim. You can\'t discern colors in darkness.\n-Gain proficiency in the intimidation skill.\n-When dropping to 0 hit points but not killed outright, you can instead go to 1 hit point (Once after every long rest).\n-When achieving a critical hit with a melee weapon, roll one of the weapon's damage dice and add that as damage to the critical hit.";
     desc[8][4] = "-Has \"DarkVision\" and can see upto 60ft in dim light as though it was bright and in darkness as if it was dim. You can\'t discern colors in darkness.\n-Resistance to fire damage.\n-Know the Thaumaturgy cantrip at lvl 1, Hellish Rebuke can be casted once per day as a 2nd level spell, at lvl 3, and the Darkness spell once per day at lvl 5 (all with CHA as the spellcasting ability).";
 
 
@@ -307,6 +310,9 @@ void Race(Character& player)
       std::cout << "Options:\n\t>N - next\n\t>P - previous\n\t>S - select\nInput: ";
       std::cin >> input;
 
+      //Allows for lowercase input of the prementioned options
+      input = toupper(input);
+
       switch(input)
       {
         case 'N':
@@ -316,11 +322,24 @@ void Race(Character& player)
           i--;
           break;
         case 'S':
-          player.SetRace(desc[i][0]);
-          //Add details for Racial Bonuses
+          if(i == 4 || i >=6)
+          {
+            player.SetRace(desc[i][0]);
+            player.AddStr(bonus[i][0]);
+            player.AddDex(bonus[i][1]);
+            player.AddCon(bonus[i][2]);
+            player.AddInt(bonus[i][3]);
+            player.AddWis(bonus[i][4]);
+            player.AddCha(bonus[i][5]);
+          }
+          else
+          {
+            //Subraces go here
+          }
           break;
         default:
-          std::cout << "Invalid Entry\n";
+          std::cout << "\n\nInvalid Entry\n";
+          system("pause");
           //Add error handling
       }
 
